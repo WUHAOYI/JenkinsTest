06aea67ea08ff055a44da0200a1380cb4d604a06
WHY <1620283589@qq.com>
2024-09-23 13:44:28 +0800

    test hook

--- src/main/java/com/why/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/why/jenkinstest/JenkinsTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello world1");
+        System.out.println("hello world12");
         SpringApplication.run(JenkinsTestApplication.class, args);
     }
 
